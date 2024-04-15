aca88de1425c249862d94801d518c2a315bdbeaa
Ahira Adefokun <justiceahira@gmail.com>
2024-04-15 13:44:50 +0100

    Fix connection settings for sqlite

--- app/common/domain/database.py
+++ app/common/domain/database.py
@@ -2,9 +2,12 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-from app.common.domain.config import SQLALCHEMY_DATABASE_URL
+from app.common.domain.config import SQLALCHEMY_DATABASE_URL, database_type
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+if database_type() == "sqlite":
+    engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
